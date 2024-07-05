21699990ccb8aa270a9231c1ef5a757e1fdfb68d
Fadilix <iamfadile2@gmail.com>
2024-07-05 15:46:58 +0000

    build settings

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import Navbar from "@/components/Navbar";
 import SaaSLandingImage from "../assets/image.png";
 import Image from "next/image";
 export default function Home() {
-  console.log(localStorage.getItem("date"));
   return (
     <main className="overflow-x-hidden grid-background">
       <Navbar />
       <div className="flex min-h-[110vh] min-w-[100vw] items-center justify-center flex-col gap-5">
         <LandingInfo />
-        <Image about="FDSQF" src={SaaSLandingImage} alt="alt" width={900} height={900} className="rounded-xl border-gray-300 border"/>
+        <Image about="FDSQF" src={SaaSLandingImage} alt="alt" width={900} height={900} className="rounded-xl border-gray-300 border" />
       </div>
     </main>
   );
