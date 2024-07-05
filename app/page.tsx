739e02d9f156e95ac4bf8483360286e0d430e733
Fadilix <iamfadile2@gmail.com>
2024-07-05 16:07:26 +0000

    build settings

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import LandingInfo from "@/components/LandingInfo";
 import Navbar from "@/components/Navbar";
 import SaaSLandingImage from "../assets/image.png";
 import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="overflow-x-hidden grid-background">
       <Navbar />
       <div className="flex min-h-[110vh] min-w-[100vw] items-center justify-center flex-col gap-5">
         <LandingInfo />
-        <Image about="FDSQF" src={SaaSLandingImage} alt="alt" width={900} height={900} className="rounded-xl border-gray-300 border" />
+        <Image about="asdf" src={SaaSLandingImage} alt="alt" width={900} height={900} className="rounded-xl border-gray-300 border" />
       </div>
     </main>
   );
